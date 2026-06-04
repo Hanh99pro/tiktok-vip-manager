231a73124ce17c4dc7acf610060b8ac722c12247
Hanh99pro <halostore99@gmail.com>
2026-06-04 22:06:39 +0700

    fix render port

--- app.py
+++ app.py
@@ -382,5 +382,8 @@ def stats():
 @app.route("/tiktokBI9SEdpBUc9JELHj5ICi79UneIrqjIBS.txt")
 def tiktok_verify_file():
     return "tiktok-developers-site-verification=BI9SEdpBUc9JELHj5ICi79UneIrqjIBS"
+# if __name__ == "__main__":
+#     app.run(host="127.0.0.1", port=5000, debug=False)
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
